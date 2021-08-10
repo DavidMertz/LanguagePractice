bc9d013477f66f4affe2bbea94ea73de3f07bffd
David Mertz <mertz@gnosis.cx>
2021-08-10 12:05:08 -0400

    Remove golang short flag template

--- golang/find-dups/find-dups.go
+++ golang/find-dups/find-dups.go
@@ -99,20 +99,6 @@ func ShowDups(sizes map[int64][]Finfo, dupsizes []int64,
     }
 }
 
-/*
-var myFlagType string
-
-func init() {
-    const (
-        flagValue = "default value is foo"
-        flagUsage = "this is my flag explanation"
-    )
-    flag.StringVar(&myFlagType, "foo", flagValue, flagUsage)
-    flag.StringVar(&myFlagType, "f", flagValue, flagUsage+" (shorthand)")
-    flag.Parse()
-}
-
-*/
 func main() {
     sizeOnly := *flag.Int("size-only", 1e9,
         "Files match if same-size larger than size-only")
