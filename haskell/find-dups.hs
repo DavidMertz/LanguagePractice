e2b5d11501fa7d0828bc606ee0459db8bbd0ab7f
David Mertz <mertz@gnosis.cx>
2021-08-19 09:49:06 -0400

    Snapshot of not-quite-right Haskell map versioning

--- haskell/find-dups.hs
+++ haskell/find-dups.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 module Main (main) where
 
 import Crypto.Hash 
@@ -27,52 +28,83 @@ stringFromSha1 (Sha1 s) = s
 
 -- File-info record
 data Finfo = Finfo { 
-  fname :: String, 
-  f_size :: Int, -- size of file
-  sha1 :: String } deriving (Show, Eq)
+    fname :: String, 
+    f_size :: Int, -- size of file
+    sha1 :: Digest SHA1 } deriving (Show, Eq)
 
 type BySize = Map Int [Finfo]
 type ByHash = Map String [Finfo]
 
+-- Compute a new map with this file entry added
+addBySize :: Int -> Finfo -> BySize -> BySize
+addBySize size finfo bySize = 
+    case (Map.lookup size bySize) of
+        Nothing -> Map.insert size [finfo] bySize
+        Just sizeEntry -> 
+            Map.adjust (\_ -> (finfo : sizeEntry)) size bySize
+
 -- Recipe from https://www.schoolofhaskell.com
 absolutize :: String -> IO String
 absolutize aPath 
-    | "~" `isPrefixOf` aPath = do
-        homePath <- getHomeDirectory
-        return $ normalise $ addTrailingPathSeparator homePath 
-                             ++ tail aPath
-    | otherwise = do
-        pathMaybeWithDots <- absolute_path aPath
-        return $ fromJust $ guess_dotdot pathMaybeWithDots
+  | "~" `isPrefixOf` aPath = do
+    homePath <- getHomeDirectory
+    return $ normalise $ addTrailingPathSeparator homePath 
+                           ++ tail aPath
+  | otherwise = do
+    pathMaybeWithDots <- absolute_path aPath
+    return $ fromJust $ guess_dotdot pathMaybeWithDots
 
--- Recipe from StackOverflow
+-- The size of a file given its path
 getFileSize :: String -> IO Int
 getFileSize path = do
     stat <- do getFileStatus path
     return $ fromIntegral (fileSize stat)
 
 -- The logic of processing files    
-processPath :: String -> IO ()
-processPath path = do
-  abspath <- do absolutize path
-  size <- do getFileSize abspath
-  content <- Bytes.readFile abspath
-  --let sha1 = hashWith SHA1 content
-  let sha1 = sha1FromString "NO HASH"
-  putStrLn (abspath ++ " " ++ show size ++ " " ++ show sha1)
+processPath :: String -> BySize -> IO (Finfo)
+processPath path bySize = do
+    !abspath <- do absolutize path
+    !size <- do getFileSize abspath
+    content <- Bytes.readFile abspath
+    let sha1 = hashWith SHA1 content
+    let finfo = Finfo abspath size sha1
+    return finfo
+
+sizeHist :: State [bySize]
+sizeHist = do
+    by_size <- get()
+    put by_size
+    get
+
 
 -- Walk directories and return two maps
-walkDir :: String -> IO (BySize)
-walkDir rootdir = do
-  pathWalk rootdir $ \root dirs files -> do
-    forM_ files (\file -> processPath (joinPath [root, file]))
-  return Map.empty
+walkDir :: String -> BySize -> IO ([BySize])
+walkDir rootdir bySize = do
+    let !bySizeHist = [bySize]
+    pathWalk rootdir (\root dirs files -> do
+        forM_ files $ (\file -> do
+            let !latest = head bySizeHist
+            finfo <- do processPath (joinPath [root, file]) latest
+            let !new = addBySize (f_size finfo) finfo latest
+
+            let latest_size = Map.keys latest
+            let new_size = Map.keys new
+            let error = if latest == new 
+                then 
+                    "Error, identical maps!" 
+                else 
+                    "Update of map is fine" ++ (show latest_size) ++ (show new_size)
+            putStrLn error
+
+            let !bySizeHist = [new] ++ bySizeHist
+            putStrLn (fname finfo) ))
+    return bySizeHist
 
 -- The "main" function walks directory with callback  
 main :: IO ()
 main = do
   rawArgs <- getArgs
   let rootdir = if rawArgs == [] then "." else head rawArgs
-  result <- walkDir rootdir
+  !result <- walkDir rootdir Map.empty
 
   putStrLn (show result)
